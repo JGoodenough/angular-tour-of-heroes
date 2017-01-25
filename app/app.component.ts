94253087f5ed827a61051254eb04f48c85b848f8
Jesse Goodenough <jessegoodenough@Jesses-MBP.home>
2017-01-25 08:11:06 -0500

    added heroes route

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   	<h1>{{title}}</h1>
+  	<a routerLink="/heroes">Heroes</a>
+  	<router-outlet></router-outlet>
   	<my-heroes></my-heroes>
   `
   ,
@@ -13,6 +15,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent { 
+	
 	title = 'Tour of Heroes';
 	
 }
