24af9e419038aa831c593a43582b194f097a1fe8
Jesse Goodenough <jessegoodenough@Jesses-MBP.home>
2017-01-25 14:36:51 -0500

    added additional styles

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -6,11 +6,13 @@ import { HeroService } from './hero.service';
 @Component({
 	moduleId: module.id,
 	selector: 'my-dashboard',
-	templateUrl: 'dashboard.component.html'
+	templateUrl: 'dashboard.component.html',
+	styleUrls: ['dashboard.component.css']
+
 })
 
 export class DashboardComponent implements OnInit {
-	
+
 	heroes: Hero[] = [];
 
 	constructor(private heroService: HeroService) { 
