0eac3151ad0d3441c16d57d0713d8ea66dfb37b6
Jesse Goodenough <jessegoodenough@Jesses-MBP.home>
2017-01-25 15:29:13 -0500

    adding http web api fake service

---
+++ app/in-memory-data.service.ts
@@ -0,0 +1,18 @@
+import { InMemoryDbService } form 'angular-in-memory-web-api';
+export class InMemoryDataService implements InMemoryDbService {
+	createDb() {
+		let heroes = [
+			{ id: 11, name: 'Batman'},
+			{ id: 12, name: 'Zelda'},
+			{ id: 13, name: 'Bombasto'},
+			{ id: 14, name: 'Celeritas'},
+			{ id: 15, name: 'Magneta'},
+			{ id: 16, name: 'RubberMan'}, 
+			{ id: 17, name: 'Superman'},
+			{ id: 18, name: 'Ultra'},
+			{ id: 19, name: 'ZEN'},
+			{ id: 20, name: 'RELAX'},
+		];
+		return {heroes};
+	}
+}
