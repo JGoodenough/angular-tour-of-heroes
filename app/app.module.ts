ac883a398a0e01570a5e84d27ecb670cf9e147d0
Jesse Goodenough <jessegoodenough@Jesses-MBP.home>
2017-01-25 11:12:34 -0500

    adding dashboard to app

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,8 +6,8 @@ import { RouterModule } from '@angular/router';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
-
 import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports:      [ 
@@ -15,15 +15,25 @@ import { HeroService } from './hero.service';
   	FormsModule,
 	RouterModule.forRoot([
 		{
+			path: '',
+			redirectTo: '/dashboard',
+			pathMatch: 'full'
+		},
+		{
 			path: 'heroes',
 			component: HeroesComponent
+		},
+		{
+			path: 'dashboard',
+			component: DashboardComponent
 		}
 	])
   ],
   declarations: [ 
   	AppComponent ,
   	HeroesComponent,
-  	HeroDetailComponent
+  	HeroDetailComponent,
+  	DashboardComponent
   ],
   providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
